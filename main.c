437cde899edb426f4bf2a0d5e625e813fb970e38
skylargartamaker <skylargartamaker1998@gmail.com>
2026-05-16 00:04:06 -0600

    Added a guessing loop

--- main.c
+++ main.c
@@ -1,63 +1,80 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define TOTAL_CHARS 36
 
-char std_alphabet[TOTAL_CHARS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-                         'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-                         'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                         '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+char std_alphabet[TOTAL_CHARS] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+    'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+};
 
-char* morse_alphabet[TOTAL_CHARS] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
-                            "....", "..", ".---", "-.-", ".-..", "--", "-.",
-                            "---", "--.-", ".-.", "-", "..-", "...-", "-.--",
-                            "--..", ".----", "..---", "...--", "....-", ".....",
-                            "-....", "--...", "---..", "----.", "-----"};
-
-char* std_to_morse(char letter)
-{
-  for (int i = 0; i < TOTAL_CHARS; i++)
-  {
-    if (toupper(letter) == std_alphabet[i])
-    {
-      return morse_alphabet[i];
-    }
-  }
-
- return " ";
-}
+char* morse_alphabet[TOTAL_CHARS] = {
+    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+    "....", "..", ".---", "-.-", ".-..", "--", "-.",
+    "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+    "...-", ".--", "-..-", "-.--","--..",
+    ".----", "..---", "...--", "....-", ".....",
+    "-....", "--...", "---..", "----.", "-----"
+};
 
 char morse_to_std(char* morse)
 {
-  for (int i = 0; i < TOTAL_CHARS; i++)
-  {
-    if (strcmp(morse, morse_alphabet[i]) == 0)
+    for (int i = 0; i < TOTAL_CHARS; i++)
     {
-      return std_alphabet[i];
+        if (strcmp(morse, morse_alphabet[i]) == 0)
+        {
+            return std_alphabet[i];
+        }
     }
-  }
 
-  return ' ';
+    return ' ';
+}
+
+void practice_loop()
+{
+    printf("Welcome to morse code practice\n");
+    printf("Type 'quit' to exit.\n\n");
+
+    char user_guess[16];
+    char letter;
+
+    while (1)
+    {
+        int random_num = rand() % TOTAL_CHARS;
+        letter = std_alphabet[random_num];
+
+        printf("What is %c?\n", letter);
+
+        scanf("%15s", user_guess);
+
+        if (strcmp(user_guess, "quit") == 0)
+        {
+            break;
+        }
+
+        char validate = morse_to_std(user_guess);
+
+        if (validate == letter)
+        {
+            printf("Correct!\n\n");
+        }
+        else
+        {
+            printf("Incorrect. Correct answer: %s\n\n",
+                   morse_alphabet[random_num]);
+        }
+    }
 }
 
 int main()
 {
-  char morse[16];
-  printf("Enter a letter in morse: ");
-  scanf("%s", morse);
+    srand(time(NULL));
+    practice_loop();
 
-  char std_alpha = morse_to_std(morse);
-
-  if (std_alpha == ' ')
-  {
-    printf("Not a valid letter\n");
-  }
-  else
-  {
-    printf("%c\n", std_alpha);
-  }
-  
-  
-  return 0;
+    return 0;
 }
